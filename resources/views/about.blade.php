69877bc7b72d9e13ab3103ab04d9618cb6122ab0
ChristianPaulo389 <cdagupan20@gmail.com>
2025-09-21 14:34:57 +0800

    Chapter 2: Implement dynamic job listings with Models

--- resources/views/about.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layout>
-    <x-slot:heading>
-        Home Page
-    </x-slot:heading>
-
-    <p class="text-gray-700">This is the About Page with Tailwind styling</p>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Homepage
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+use App\Models\Job;
+
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            ['id' => 1, 'title' => 'Director', 'salary' => '$50,000'],
+            ['id' => 2, 'title' => 'Programmer', 'salary' => '$10,000'],
+            ['id' => 3, 'title' => 'Teacher', 'salary' => '$40,000'],
+        ]
+    ]);
 });
 
-Route::get('/contact', function () {
-    return view('contact');
+
+Route::get('/jobs/{id}', function ($id) {
+    return view('job', ['job' => Job::find($id)]);
 });
+
