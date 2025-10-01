78d1fdd2b9c66fe4e67e14d2fbd1e0bd01e4869a
ChristianPaulo389 <cdagupan20@gmail.com>
2025-10-01 22:20:46 +0800

    Chapter 2: Implement dynamic job listings with Models

--- resources/views/about.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layout>
-    <x-slot:heading>
-        About Us
-    </x-slot:heading>
-
-    <p>This is the About page content.</p>
-</x-layout>
--- resources/views/contact.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layout>
-    <x-slot:heading>
-        Contact
-    </x-slot:heading>
-
-    <p>You can reach us at email@example.com</p>
-</x-layout>
